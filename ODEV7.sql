76d111a3c5c9dfbd283926aa1b73e0b90f858f30
SerdarSelli <53913443+SerdarSelli@users.noreply.github.com>
2023-03-01 15:53:04 +0300

    Update ODEV7.sql

--- ODEV7.sql
+++ ODEV7.sql
@@ -10,3 +10,15 @@ SELECT replacement_cost, COUNT(*) FROM film
 GROUP BY replacement_cost
 HAVING COUNT(*) > 50
 ORDER BY replacement_cost ;
+
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+SELECT store_id, COUNT(*) FROM customer
+GROUP BY store_id
+
+--4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini
+--ve şehir sayısını paylaşınız.
+
+SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1 ;
